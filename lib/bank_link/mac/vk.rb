b6471d7f5063f393d29d67d299bc4efd1b829e6e
Tomas Aleksiejunas <taleksiejunas@gmail.com>
2015-03-05 13:45:31 +0200

    Update vk.rb

--- lib/bank_link/mac/vk.rb
+++ lib/bank_link/mac/vk.rb
@@ -39,7 +39,7 @@ module BankLink
 
       def request_data version, type=:request
         keys(version, type).collect { |key_name|
-          field_for data[key_name].to_s unless data[key_name].nil?
+          field_for data[key_name].to_s if !data[key_name].nil? || type == :response
         }.flatten.join
       end
     end
